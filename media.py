c5c828a236497e6168c0bfac3030fc70b49ddfc0
capt-marwil <mrgwillemsen@gmail.com>
2015-10-11 10:12:50 +0200

    feat: Add derived Class tv Shows

--- media.py
+++ media.py
@@ -6,38 +6,42 @@ import webbrowser
 class Digital_asset(object):
     """ A general class to represent Digital assets such as Movies, TV-shows, Books, Games etc.
     """
-    def __init__(self, title, synopsis, rating):
+    def __init__(self, title, synopsis, rating, poster_image_url, trailer_youtube_url):
         self.title = title
         self.synopsis = synopsis
         self.rating = rating
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
 
 
 class Movie(Digital_asset):
     """A class to store and display information on movies"""
 
     def __init__(self, title, synopsis, rating, movie_storyline, poster_image_url, trailer_youtube_url, director, cast):
-        Digital_asset.__init__(self, title, synopsis, rating)
+        Digital_asset.__init__(self, title, synopsis, rating, poster_image_url, trailer_youtube_url)
         self.movie_storyline = movie_storyline
-        self.poster_image_url = poster_image_url
-        self.trailer_youtube_url = trailer_youtube_url
         self.director = director
-        self.cast = cast
+        self.cast = cast.split(',')
 
-    def get_title(self):
-        return self.title
-
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
-
-    def show_poster(self):
-        webbrowser.open(self.poster_image)
+    def get_cast(self):
+        cast = ""
+        for i in self.cast:
+            cast = cast + i + "<br>"
+        return cast
 
 
 class TvShow(Digital_asset):
 
-    def __init__(self, title, synopsis, rating, tv_station):
-        Digital_asset.__init__(title, synopsis, rating)
+    def __init__(self, title, synopsis, rating, tv_station, poster_image_url, trailer_youtube_url, cast):
+        Digital_asset.__init__(self, title, synopsis, rating, poster_image_url, trailer_youtube_url)
         self.tv_station = tv_station
+        self.cast = cast.split(',')
+
+    def get_cast(self):
+        cast = ""
+        for i in self.cast:
+            cast = cast + i + "<br>"
+        return cast
 
 
 
